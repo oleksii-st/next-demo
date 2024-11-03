d46da42059eaa52e4562abe8030846da40021995
Oleksii S <stokrockyioleksii@gmail.com>
2024-11-03 20:24:36 +0200

    fix: remove unused tailwind paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/blocks/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
