e84a7235fc4f2f8171b8b3c117300bd440937f90
Oleksii S <stokrockyioleksii@gmail.com>
2024-11-03 20:34:16 +0200

    fix: remove unused styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export const Layout = async ({ children }: { children: ReactNode }) => {
       <Header />
       <main
         className={cn(
-          'prose prose-basic prose-code:text-[var(--code-inline-color)] prose-code:bg-[var(--code-inline-bg-color)] prose-code:before:content-none prose-code:after:content-none prose-code:px-0.5 prose-code:py-1 prose-h2:mt-0',
+          'prose prose-basic prose-code:before:content-none prose-code:after:content-none prose-code:px-0.5 prose-code:py-1 prose-h2:mt-0',
           'flex-auto flex flex-col max-w-full',
         )}
         id="main"
